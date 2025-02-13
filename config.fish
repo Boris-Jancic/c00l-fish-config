5cadaede3a2abc4938344c86751000c17c7fa86f
Boris Jancic <34841404+Boris-Jancic@users.noreply.github.com>
2025-02-13 13:36:11 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,12 +2,38 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# ========= Thefuck -> terminal mistake corrector 
 thefuck --alias | source
 
-# ~/.config/fish/config.fish
 
-# Initializes starship for more pretty terminal
+# ========= Starship -> more pretty terminal
 starship init fish | source
 
 # Replaces default ls command with more collorfull & descriptive
-alias ls "eza --color=always --long --git --no-filesize --icons=always --no-user --tree --level=2"
+alias ls "eza --color=always --long --git --icons=always --no-user --tree"
+
+
+# ========= Zoxide -> better cd command
+
+# Inits zoxide if it was not initialized
+if command -sq zoxide
+    zoxide init fish | source
+else
+    echo 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
+end
+
+# Replaces defauld cd command with zoxide
+alias z="ls"
+
+
+# ========= Fzf -> fuzzy finder
+
+eval "$(fzf --fish)"
+
+
+# ========= Git aliases
+alias glo="git log --graph --oneline --decorate"
+alias gs="git status"
+alias gcm="git commit -m"
+alias gcam="git commit -am"
+alias gc="git checkout
